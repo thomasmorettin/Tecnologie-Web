5ae9b343d5eed64348afe0ffcd79a80b117c36e3
thomasmorettin <thomas.morettin@gmail.com>
2025-10-29 22:51:33 +0100

    Inserimento pulsante go-back-to-top e migliorie responsivness.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,23 +1,56 @@
 function updateTitleIcon() {
   const icnTitle = document.getElementById("title-icon");
   if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    icnTitle.href = "Resources/Vectors/LogoRidotto_Light.svg";
+    icnTitle.href = "../Resources/Vectors/LogoRidotto_Light.svg";
   } else {
-    icnTitle.href = "Resources/Vectors/LogoRidotto.svg";
+    icnTitle.href = "../Resources/Vectors/LogoRidotto.svg";
   }
 }
 
-updateTitleIcon();    // Avvio della pagina web
+function headerScroll() {
+  const header = document.querySelector("header");
+  if (header) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  }
+}
 
+function showBtnBackToTop() {
+  const backToTop = document.getElementById("btn-back-to-top");
+  if (backToTop) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        backToTop.classList.add("visible");
+      } else {
+        backToTop.classList.remove("visible");
+      }
+    });
+  }
+}
+
+function backToTop() {
+  window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+function btnHamburgerClick() {
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.getElementById("menu");
+
+  hamburger.addEventListener("click", function() {
+    nav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  })
+}
+
+updateTitleIcon();    // Avvio della pagina web
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateTitleIcon);    // Aggiornamento automatico
 
-const header = document.querySelector("header");
-if (header) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
-}
+headerScroll();   // Modifica dello stile dell'header allo scorrimento della pagina
+showBtnBackToTop();   // Mostra il pulsante per ritornare in cima alla pagina
+
+btnHamburgerClick();    // Click del menù hamburger in formato mobile
